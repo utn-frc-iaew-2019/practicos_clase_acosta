1ab84744f78feaf69f097f05e0b59f121c926e8b
facuvillard <facuvillard@gmail.com>
2019-04-12 17:53:00 -0300

    clase 12-04

---
+++ Clase 12-04/clase12-04/src/main/java/clase12_04/clase12_04/App.java
@@ -0,0 +1,13 @@
+package clase12_04.clase12_04;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
